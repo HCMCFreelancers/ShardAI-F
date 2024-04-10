956020c668d81228d784468f1577986da0751ce6
dawngn <haidangdavid123@gmail.com>
2024-04-10 10:13:11 +0700

    feat: update hero& header

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config, { isServer }) {
-    const prefix = config.assetPrefix ?? config.basePath ?? "";
     config.module.rules.push({
       test: /\.(webm|mp4)$/,
       use: [
--- src/components/enhance-mining/enhance-mining.js
+++ src/components/enhance-mining/enhance-mining.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import ai from '@/images/enhance-mining/ai.mp4'
-import frame from '@/images/enhance-mining/frame.png'
+
 
 const EnhanceMining = () => {
   return (
